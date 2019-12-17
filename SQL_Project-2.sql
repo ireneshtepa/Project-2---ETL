a35ace7ab9686227b398937b73cb6992679f0d6c
Yatish-Mullaji <52358004+Yatish-Mullaji@users.noreply.github.com>
2019-12-16 20:18:00 -0500

    Final update

--- SQL_Project-2.sql
+++ SQL_Project-2.sql
@@ -1,14 +1,20 @@
-DROP TABLE movies
+DROP TABLE movies;
 CREATE TABLE movies (
-	Year INT
-	,Movie VARCHAR PRIMARY KEY
-	,Director VARCHAR 
-	,Studio VARCHAR 
+	"Year" INT
+	,"Movie" VARCHAR PRIMARY KEY
+	,"Director" VARCHAR 
+	,"Studio" VARCHAR 
 )
 SELECT * FROM movies;
 
+DROP TABLE ratings;
 CREATE TABLE ratings (
-	Movie VARCHAR 
-	,Rating float
+	"Movie" VARCHAR 
+	,"Rating" float
 )
 SELECT * FROM ratings;
+
+SELECT movies."Year", movies."Movie", movies."Director", movies."Studio", ratings."Rating" 
+FROM movies 
+JOIN ratings 
+ON movies."Movie" = ratings."Movie";
